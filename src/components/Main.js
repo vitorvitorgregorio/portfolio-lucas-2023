a40fea6c22c33bc597066beb24d55687dade03f5
vitorvitorgregorio <vitorvitorgregorio@gmail.com>
2023-04-18 20:44:36 -0300

    mais algumas alterações

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,13 +1,24 @@
 import "../styles/sass/Main.sass";
-import queijoHover from "../images/queijo-e-prosa-hover.png";
-import voltageHover from "../images/voltage-hover.png";
-import omegaHover from "../images/omega-energia-hover.png";
+//experiências digitais
+import brasilDentalEx from "../images/experiencias-digitais/brasil-dental-ex.png";
+import fineEx from "../images/experiencias-digitais/fine-ex.png";
+import OmegaEnergiaEx from "../images/experiencias-digitais/omega-energia-ex.png";
+//Marcas
+import omegaMarcas from "../images/marcas/omega-marcas.png";
+import queijoMarcas from "../images/marcas/queijo-e-prosa-marcas.png";
+import voltageMarcas from "../images/marcas/voltage-marcas.png";
+//companhias
+import ciclicCompanhias from "../images/companhias/ciclic.png";
+import heinekenCompanhias from "../images/companhias/heineken.png";
+import btgCompanhias from "../images/companhias/btg.png";
+import omegaCompanhias from "../images/companhias/omega.png";
+import queijoCompanhias from "../images/companhias/queijo-e-prosa.png";
+import BrasilDentalCompanhias from "../images/companhias/brasil-dental.png";
+
 import React, { useState } from "react";
 function Main() {
-  const [isShown, setIsShown] = useState(false);
   const [cursorX, setCursorX] = useState();
   const [cursorY, setCursorY] = useState();
-
   window.addEventListener("mousemove", (e) => {
     setCursorX(e.pageX);
     setCursorY(e.pageY);
@@ -24,47 +35,80 @@ function Main() {
           }}
         ></div>
         <span className="main__title-normal">
-          Product Design Lead baseado em São Paulo, construindo{" "}
+          Product Design Lead baseado em São Paulo, construindo
         </span>
 
         <span className="main__title-hover experiencias-digitais">
           experiências digitais,
+          <div className="box-hover experiencias-digitais">
+            <div className="image-box">
+              <img src={brasilDentalEx} alt="" />
+              <p>brasil Dental</p>
+            </div>
+            <div className="image-box">
+              <img src={fineEx} alt="" />
+              <p>FineApp</p>
+            </div>
+            <div className="image-box">
+              <img src={OmegaEnergiaEx} alt="" />
+              <p>Omega Energia</p>
+            </div>
+          </div>
         </span>
 
         <span className="main__title-normal">estratégia e </span>
-        <span
-          className="main__title-hover marcas"
-          onMouseEnter={() => setIsShown(true)}
-          onMouseLeave={() => setIsShown(false)}
-        >
+        <span className="main__title-hover marcas">
           marcas
-          {isShown && (
-            <div className="box-hover marcas-hover">
-              <img src={queijoHover} alt="" className="delay" />
-              <img src={voltageHover} alt="" className="delay" />
-              <img src={omegaHover} alt="" className="delay" />
+          <div className="box-hover marcas">
+            <div className="image-box">
+              <img src={omegaMarcas} alt="" />
+              <p>Omega energia</p>
             </div>
-          )}
+            <div className="image-box">
+              <img src={queijoMarcas} alt="" />
+              <p>Queijo & Prosa</p>
+            </div>
+            <div className="image-box">
+              <img src={voltageMarcas} alt="" />
+              <p>Voltage</p>
+            </div>
+          </div>
         </span>
         <span className="main__title-normal">para</span>
-        <span
-          className="main__title-hover companhias"
-          onMouseEnter={() => setIsShown(true)}
-          onMouseLeave={() => setIsShown(false)}
-        >
+        <span className="main__title-hover companhias">
           companhias
-          {isShown && (
-            <div className="box-hover marcas-hover">
-              <img src={queijoHover} alt="" className="delay" />
-              <img src={voltageHover} alt="" className="delay" />
-              <img src={omegaHover} alt="" className="delay" />
+          <div className="box-hover companhias">
+            <div className="image-box">
+              <img src={ciclicCompanhias} alt="" />
+              <p>Ciclic</p>
             </div>
-          )}
+            <div className="image-box">
+              <img src={heinekenCompanhias} alt="" />
+              <p>Heineken</p>
+            </div>
+            <div className="image-box">
+              <img src={btgCompanhias} alt="" />
+              <p>BTG Pactual</p>
+            </div>
+            <div className="image-box">
+              <img src={BrasilDentalCompanhias} alt="" />
+              <p>Banco do Brasil</p>
+            </div>
+            <div className="image-box">
+              <img src={queijoCompanhias} alt="" />
+              <p>Queijo & Prosa</p>
+            </div>
+            <div className="image-box">
+              <img src={omegaCompanhias} alt="" />
+              <p>Omega energia</p>
+            </div>
+          </div>
         </span>
         <span className="main__title-normal">
           que impactam positivamente a vida das pessoas.
         </span>
       </h1>
+
       <h1 className="main__title-mobile">
         Product Design Lead baseado em São Paulo, construindo experiências
         digitais, estratégia e marcas para companhias que impactam positivamente
